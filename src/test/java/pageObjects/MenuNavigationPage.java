346e69cb7718dae3dc9810c80b091c00e83bfbea
My Name <vvmelnichenko@miem.hse.ru>
2023-08-22 19:25:31 +0300

    Сделаны аннотации степ для существующих тестов. Сделан параметризованный тест.

--- src/test/java/pageObjects/MenuNavigationPage.java
+++ src/test/java/pageObjects/MenuNavigationPage.java
@@ -2,6 +2,7 @@ package pageObjects;
 
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import java.util.List;
@@ -17,20 +18,19 @@ public class BeautyAndSport {
     private static String beautyAndSportSubcatalogLocator = ".subcategory-item.mobile-modal[data-name='%s'] ul.menu-list li.menu-item";
 
 
+    @Step("Получены все подкаталоги, расположенные в разделе 'Красота и спорт'.")
     public static List<String> getBeautyAndSportCatalog() {
         List<SelenideElement> beautyAndSportCatalogList = $$(beautyAndSportCatalogLocator);
-        List<String> catalogNames = beautyAndSportCatalogList.stream().map(element -> element.innerText()).collect(Collectors.toList());
+        List<String> catalogNames = beautyAndSportCatalogList.stream().map(element -> element.innerText().trim()).collect(Collectors.toList());
         return catalogNames;
     }
 
+    @Step("")
     public static List<String> getbeautyAndSportSubcatalog(String subcatalogName) {
         String beautyAndSportSubcatalog = String.format(beautyAndSportSubcatalogLocator, subcatalogName);
         List<SelenideElement> catatogList = $$(beautyAndSportSubcatalog);
-        List<String> catalogNames = catatogList.stream().map(element -> element.innerText()).collect(Collectors.toList());
+        List<String> catalogNames = catatogList.stream().map(element -> element.innerText().trim()).collect(Collectors.toList());
         return catalogNames;
     }
-
-
-
 
 }
